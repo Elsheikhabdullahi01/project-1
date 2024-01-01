de8d2bc7f4113bea44deaad887f8aaee79795a3f
Elsheikhabdullahi01 <155262957+Elsheikhabdullahi01@users.noreply.github.com>
2024-01-01 21:59:49 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,75 +1,38 @@
-from kivymd.app import MDApp
+from kivy.app import App
+from kivy.uix.widget import Widget
 from kivy.lang import Builder
 
+Builder.load_string('''
+<Example>:
+    id: my_id
+    BoxLayout:
+        orientation: 'vertical'
+        size: root.width, root.height
+        spacing: 40
+        padding: 50
+        
+        Image:
+            id: im
+            source: ' '
+        FileChooserListView:
+            id: chose
+            on_selection: root.I(chose.selection)
+        Label:
+            id: la
+            text: 'Choose Image'
+            size_hint: (1,0.2)
+            font_size: 35
 
-
-  
-class MyApp(MDApp):
-    
+''')
+class Example(Widget):
+    def I(self, file):
+        try:
+            self.ids.im.source = file[0]
+            self.ids.la.text ='You Choose: \n '+ str(file)
+        except:
+            pass
+class MyApp(App):
     def build(self):
-        self.theme_cls.theme_style = 'Dark'
-        self.theme_cls.primary_palette = 'Brown'
-        return Builder.load_string('''
-<My@MDSwiperItem>:
-    #id: my
-MDScreen:
-#    id: my
-
-    MDTopAppBar:
-      #  id: my
-        left_action_items: [['menu']]
-        id: my
-        title: 'Galary'.upper()
-        pos_hint: {'top':1}
-        
-    MDSwiper:  
-      #  id: my  
-        size_hint_y: None
-        height: root.height - my.height - dp(30)
-        y:root.height - self.height - my.height - dp(20)
-        
-        My:
-       #     
-            BoxLayout:
-                #id: my
-                size: self.width, self.height
-                spacing: 10
-                padding: 20
-                orientation: 'vertical'
-                
-                Image:
-                    id: im
-                    source: ' '
-                    
-                FileChooserIconView:
-                    id: c
-                    on_selection: app.Se(c.selection)
-                
-        My:
-            FitImage:
-                source: 'IMG-20230727-WA0033.jpg'
-                radius: [60]
-               
-        My:
-            FitImage:
-                source: 'FB_IMG_16775863970187907.jpg'
-                radius: [60]
-                
-        My:
-            FitImage:
-                source: 'logo.png'
-                radius: [60]
-        My:
-            FitImage:
-                source: 'status_me_status_IMG-20230416-WA0027.jpg'
-                radius: [60]
-           
-     
-''')  
-    def Se(self, file):
-      #  print(file)
-        self.root.ids.im.source = file[0]
-
-        
-        
+        return Example()
+      
 MyApp().run()
